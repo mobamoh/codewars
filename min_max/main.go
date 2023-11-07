2627e219d55516e31391df2de4151e14a4b70a2b
mobamoh <b4m0h4m3d@gmail.com>
2023-11-07 13:24:56 +0100

    min max

---
+++ min_max/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%#v\n", MinMax([]int{1, 2, 3, 4, 5}))
+	fmt.Printf("%#v\n", MinMax([]int{2334454, 5}))
+	fmt.Printf("%#v\n", MinMax([]int{-2334454, -5}))
+	fmt.Printf("%#v\n", MinMax([]int{1, 1}))
+	fmt.Printf("%#v\n", MinMax([]int{7, 33, 99, 1, 5, 2, 3, 2}))
+}
+
+func MinMax(arr []int) [2]int {
+	min, max := arr[0], arr[0]
+	for _, v := range arr {
+		if min > v {
+			min = v
+		}
+		if max < v {
+			max = v
+		}
+	}
+
+	return [2]int{min, max}
+}
